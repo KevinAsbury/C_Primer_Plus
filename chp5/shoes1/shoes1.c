782cbbd986f6c5429bc83c0f1df93e2fd58137fd
Kevin Asbury <kevinasbury.dev@gmail.com>
2019-08-15 17:01:54 -0400

    shoes

---
+++ chp5/shoes1/shoes1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define ADJUST 7.64
+#define SCALE 0.325
+
+int main(void)
+{
+    double shoe, foot;
+
+    shoe = 12.5;
+
+    foot = SCALE * shoe + ADJUST;
+    printf("Shoe size (men's)     foot length\n");
+    printf("%10.1f %15.2f inches\n", shoe, foot);
+
+    return 0;
+}
