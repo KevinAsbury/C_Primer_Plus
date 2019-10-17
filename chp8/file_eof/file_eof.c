1017a0974a32e37c272a4f2483e64b3fe0259b6f
Kevin Asbury <kevinasbury.dev@gmail.com>
2019-10-17 16:08:35 -0400

    chp8 file_eof

---
+++ chp8/file_eof/file_eof.c
@@ -0,0 +1,28 @@
+// file_eof.c --open a file and display it
+#include <stdio.h>
+#include <stdlib.h>         // for exit()
+
+int main(void)
+{
+    int ch;
+    FILE *fp;
+    char fname[50];         // hold the file name
+
+    printf("Enter the name of the file: ");
+    scanf("%s", fname);
+    fp = fopen(fname, "r"); // Open the file for reading
+
+    if (fp == NULL)         // attempt failed
+    {
+        printf("Failed to open faile. Bye\n");
+        exit(1);
+    }
+
+    // getc(fp) gets a character from the open file
+    while ((ch = getc(fp)) != EOF)
+        putchar(ch);
+
+    fclose(fp);             // close the file
+
+    return 0;
+}
