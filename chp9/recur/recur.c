942412b430eeb7e2f6b703a7ee296e44cdc608ac
Kevin Asbury <kevinasbury.dev@gmail.com>
2019-10-22 16:50:09 -0400

    chp9 recur

---
+++ chp9/recur/recur.c
@@ -0,0 +1,20 @@
+// recur.c -- recursuion illustration
+#include <stdio.h>
+
+void up_and_down(int);
+
+#define HELLO "Hello, world!"
+
+int main(void)
+{
+    up_and_down(1);
+    return 0;
+}
+
+void up_and_down(int n)
+{
+    printf("Level %d: n location %p\n", n, &n);
+    if (n < 4)
+        up_and_down(n+1);
+    printf("LEVEL %d: n location %p\n", n, &n);
+}
