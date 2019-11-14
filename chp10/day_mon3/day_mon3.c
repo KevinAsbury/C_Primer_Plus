6e08a284d0c16907bc86d51817d1956a8c26d1ec
Kevin Asbury <kevinasbury.dev@gmail.com>
2019-11-14 10:30:38 -0500

    "chp10 day_mon3"

--- chp10/day_mon3/day_mon3.c
+++ chp10/day_mon3/day_mon3.c
@@ -5,5 +5,13 @@
 int main(void)
 {
     int days[MONTHS] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    int index;
+
+    for (index = 0; index < MONTHS; index++)
+    {
+        printf("Month %2d has %d days.\n", index + 1, 
+                *(days + index));  // same as days[index]
+    }
+    
     return 0;
 }
